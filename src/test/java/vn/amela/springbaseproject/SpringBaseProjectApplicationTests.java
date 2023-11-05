2055e0880252875f062becf4a95be12f31cce642
Nguyễn Hồng Sơn <nhs3108@gmail.com>
2023-11-06 00:52:22 +0900

    Init

---
+++ src/test/java/vn/amela/springbaseproject/SpringBaseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.amela.springbaseproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBaseProjectApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
